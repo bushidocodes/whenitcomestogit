1e8feac2cd315d72028bdba0eb46e802c734ca50
Sean McBride <spmcbride1201@gmail.com>
2016-12-11 13:54:05 -0500

    Initial Commit.

---
+++ rhymes.js
@@ -0,0 +1,15 @@
+window.onload = function (e) {
+  console.log("running");
+  let msg = new SpeechSynthesisUtterance(`
+      When it comes to get, I push hard like a pusher,,
+      Cooking up in bash, something sweet for my stash,,
+      I stay committed to push code that gets you twisted,,,,
+      If you break my license, you’ll find yourself in crisis,
+      I'll hide like a dot directory
+      and start calculating trajectories
+      that will leave you headless
+
+
+      `);
+  window.speechSynthesis.speak(msg);
+}
